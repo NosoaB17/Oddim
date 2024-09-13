94302fe21229f8873682026fb54efa66b5921dac
NosoaB17 <ngsn176@gmail.com>
2024-09-13 11:21:47 +0700

    Css for user-menu in products page

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -4,12 +4,17 @@ import Logo from "../../assets/Logo.png";
 import TranslationIcon from "../../assets/navbar/Translation.svg";
 import ConversationIcon from "../../assets/navbar/Conversation.svg";
 import ExtensionIcon from "../../assets/navbar/Extension.svg";
-import { useNavigate } from "react-router-dom";
+import SignIn from "../../assets/SignIn.svg";
+import login from "../../assets/log-out.png";
+import settings from "../../assets/settings.png";
+import { Link, useNavigate } from "react-router-dom";
 
 const NavBar = () => {
-  const [activeTab, setActiveTab] = useState("Translation");
+  const [isDropdownOpen, setIsDropdownOpen] = useState(false);
   const navigate = useNavigate();
+  const toggleDropdown = () => setIsDropdownOpen(!isDropdownOpen);
 
+  const [activeTab, setActiveTab] = useState("Translation");
   const handleTabClick = (tab) => {
     setActiveTab(tab);
   };
@@ -52,7 +57,29 @@ const NavBar = () => {
           ))}
         </div>
         <div className="navbar-right">
-          <button className="icon-button">👤</button>
+          <button className="user-icon" onClick={toggleDropdown}>
+            <img src={SignIn} alt="User" />
+          </button>
+          {isDropdownOpen && (
+            <div className="dropdown">
+              <Link
+                to="/signin"
+                className="dropdown-item"
+                onClick={toggleDropdown}
+              >
+                <img src={login} alt="Sign In" />
+                Sign In
+              </Link>
+              <Link
+                to="/settings"
+                className="dropdown-item"
+                onClick={toggleDropdown}
+              >
+                <img src={settings} alt="Settings" />
+                Settings
+              </Link>
+            </div>
+          )}
         </div>
       </div>
     </nav>
