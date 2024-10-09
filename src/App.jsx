7d99573532ccab6d057cc9f72e0e66c196f02b59
NosoaB17 <ngsn176@gmail.com>
2024-10-09 13:54:26 +0700

    Remove Layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,39 @@
 import React from "react";
-import { BrowserRouter } from "react-router-dom";
-import Layout from "./Layout";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  useLocation,
+} from "react-router-dom";
+import Header from "./components/Header/Header";
+import Navbar from "./components/Navbar/Navbar";
+import Homepage from "./pages/Homepage";
+import Products from "./pages/Products";
+import Auth from "./pages/Auth";
+import Conversation from "./pages/Conversation";
 
 import "./styles/index.css";
 
+const Navigation = () => {
+  const location = useLocation();
+  const isHomePage = location.pathname === "/";
+
+  return isHomePage ? <Header /> : <Navbar />;
+};
+
 const App = () => {
   return (
-    <BrowserRouter>
-      <Layout />
-    </BrowserRouter>
+    <Router>
+      <div className="relative overflow-x-hidden">
+        <Navigation />
+        <Routes>
+          <Route path="/" element={<Homepage />} />
+          <Route path="/products" element={<Products />} />
+          <Route path="/signin" element={<Auth />} />
+          <Route path="/conversation" element={<Conversation />} />
+        </Routes>
+      </div>
+    </Router>
   );
 };
 
